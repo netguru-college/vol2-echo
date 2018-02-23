489fdb9658a88e07d5c700839b5bc64c9df17932
Bartosz Sobczynski <bartosobczynski@gmail.com>
2018-02-23 14:28:58 +0100

    Add members to league view

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20180223112412) do
     t.datetime "updated_at", null: false
   end
 
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

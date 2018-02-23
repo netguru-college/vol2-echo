c2f808647ee76f396a50b6400f6c43c6f13f2991
Bartosz Sobczynski <bartosobczynski@gmail.com>
2018-02-23 14:28:58 +0100

    Add members to league view

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20180222153215) do
     t.datetime "updated_at", null: false
   end
 
+<<<<<<< HEAD
+=======
+  create_table "leagues_members", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "league_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+>>>>>>> 489fdb9... Add members to league view
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false

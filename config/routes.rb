ca009c3fb0a2a3140d6b88c36410eaf3a5860397
Bartosz Sobczynski <bartosobczynski@gmail.com>
2018-02-23 19:23:51 +0100

    Let users to join leagues

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :leagues
+  post 'league/:id/join', to: 'leagues#join', as: 'join_league'
   root 'leagues#index'
 end

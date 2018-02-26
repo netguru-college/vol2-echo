dcc230e343b7dca5150994da55b3cf3ca71528bb
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-26 17:00:53 +0100

    Sumarize the whole day of work :)

--- app/models/league.rb
+++ app/models/league.rb
@@ -3,7 +3,6 @@ class League < ApplicationRecord
   has_many :leagues_members
   has_many :users, through: :leagues_members
   has_many :matches
-  has_many :users, through: :matches
 
   validates :name, presence: true, length: { in: 3..30 }
   validates :description, presence: true, length: { in: 5..150 }

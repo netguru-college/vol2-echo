76744626b6a30b916d83b43f2eb45fdbf107fabb
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-26 12:00:33 +0100

    Update models

--- app/models/match.rb
+++ app/models/match.rb
@@ -3,4 +3,6 @@ class Match < ApplicationRecord
   belongs_to :player1, class_name: 'User', foreign_key: 'player_1'
   belongs_to :player2, class_name: 'User', foreign_key: 'player_2'
   validates_uniqueness_of :player_2, scope: %i[league_id player_1] #not tested yet
+
+  scope :players, -> { includes(:player1, :player2) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
    has_many :leagues_members
    has_many :leagues, through: :leagues_members
+   # has_many :matches
+   #has_many :leagues, through: :matches
 
 end

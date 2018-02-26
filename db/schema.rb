b4be9faecc5bb162d55f5836704530b3197c5697
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-26 10:15:51 +0100

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226085055) do
+ActiveRecord::Schema.define(version: 20180226090941) do
 
   create_table "leagues", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20180226085055) do
     t.integer "league_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.integer "league_id"
+    t.integer "player_1"
+    t.integer "player_2"
+    t.integer "player1_goals"
+    t.integer "player2_goals"
   end
 
   create_table "users", force: :cascade do |t|

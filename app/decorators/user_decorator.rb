8611bc6dcd4191f3ff971531d05480c9f8ec0fda
Bartosz Sobczynski <bartosobczynski@gmail.com>
2018-02-27 16:58:49 +0100

    fix double quotes

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,6 +1,8 @@
 class UserDecorator < Draper::Decorator
   delegate_all
   def leagues_summarize
-    "Owns #{object.owned_leagues.count} leagues. Participes in #{object.leagues.count} leagues."
+    owns = object.owned_leagues.count
+    participates = object.leagues.count
+    "Owns #{owns} #{'league'.pluralize(owns)}. Participates in #{participates} #{'league'.pluralize(participates)}."
   end
 end

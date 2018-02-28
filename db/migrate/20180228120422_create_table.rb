cc97238772ee8102e1c630fcc83312b695439f3d
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-28 13:06:48 +0100

    Create migration to create Table

---
+++ db/migrate/20180228120422_create_table.rb
@@ -0,0 +1,16 @@
+class CreateTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tables do |t|
+      t.integer :league
+      t.integer :player
+      t.integer :played
+      t.integer :won
+      t.integer :drawn
+      t.integer :lost
+      t.integer :goals_for
+      t.integer :goals_against
+      t.integer :goal_diff
+      t.integer :points
+    end
+  end
+end

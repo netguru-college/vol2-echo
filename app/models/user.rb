84e4427282327c6f1c7331479efa4edfdf644020
Bartosz Sobczynski <bartosobczynski@gmail.com>
2018-02-28 13:25:30 +0100

    Remove whitespace

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,6 @@ class User < ApplicationRecord
   has_many :leagues_members
   has_many :leagues, through: :leagues_members
   has_many :owned_leagues, class_name: 'League', foreign_key: 'owner_id'
-  
   has_many :profile_comments, class_name: 'Comment', foreign_key: 'profile_id'
   has_many :written_comments, class_name: 'Comment', foreign_key: 'author_id'
 end

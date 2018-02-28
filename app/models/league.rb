907c12e97e83bcf20a7d3abeefc1a0f66017a319
RGu0000 <ryszardgurzeda@gmail.com>
2018-02-28 12:06:31 +0100

    Update seeds and optimise queries

--- app/models/league.rb
+++ app/models/league.rb
@@ -4,6 +4,8 @@ class League < ApplicationRecord
   has_many :users, through: :leagues_members
   has_many :matches
 
+  scope :matches, -> { includes(:users) }
+
   validates :name, presence: true, length: { in: 3..30 }, uniqueness: true
   validates :description, presence: true, length: { in: 5..150 }
 end

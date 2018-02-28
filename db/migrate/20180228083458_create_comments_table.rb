ab887616a71c892b7cbd0470d1155f11acd934e8
Bartosz Sobczynski <bartosobczynski@gmail.com>
2018-02-28 09:43:38 +0100

    fix migration

--- db/migrate/20180228083458_create_comments_table.rb
+++ db/migrate/20180228083458_create_comments_table.rb
@@ -3,9 +3,7 @@ class CreateCommentsTable < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :title
       t.text :message
-      t.references :user, index: true, foreign_key: true
-      t.references :author, index: true, foreign_key: true
       t.timestamps
     end
   end
-end
+e

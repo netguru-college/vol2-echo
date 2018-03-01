6bb65d2a3e56b42f07e0b596fc4c0fcb89cf47f2
Kacper Lakomski <Kacper.lakomski@gmail.com>
2018-03-01 11:15:23 +0100

    Ranking table added - Needs an update

--- app/decorators/match_decorator.rb
+++ app/decorators/match_decorator.rb
@@ -4,7 +4,7 @@ class MatchDecorator < Draper::Decorator
     if !object.player1_goals.nil? && !object.player2_goals.nil?
       "Score: #{player1_goals}:#{player2_goals}"
     else
-      "Match haven't played"
+      "Match hasn't been played yet"
     end
   end
 end
